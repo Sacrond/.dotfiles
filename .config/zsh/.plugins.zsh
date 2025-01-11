fd7f0326d224c82a291bf6548d3493c7e9242276
Sacrond <sacrond@gmail.com>
2025-01-11 14:39:37 +0800

    Updated plugins

--- .config/zsh/.plugins.zsh
+++ .config/zsh/.plugins.zsh
@@ -3,3 +3,9 @@ if [ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.
 else
    echo "zsh-syntax-highlighting not installed"
 fi
+
+if [ -f "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
+   source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+else
+   echo "zsh-autosuggestions not installed"
+fi
