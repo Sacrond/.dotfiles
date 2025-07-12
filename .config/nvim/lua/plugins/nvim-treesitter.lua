a43fa143f0f288adebed8e2f579cf1dd2243d06c
Sacrond <bryanphui@gmail.com>
2025-07-12 12:24:18 +0800

    feat: updated treesitter and bufferline style

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = { "HiPhish/nvim-ts-rainbow2" },
-  opts = function(_, opts)
-    opts.rainbow = {
-      enable = true,
-      query = "rainbow-parens",
-      strategy = require("ts-rainbow").strategy.global,
-      hlgroups = {
-        "TSRainbowRed",
-        "TSRainbowOrange",
-        "TSRainbowYellow",
-        "TSRainbowGreen",
-        "TSRainbowBlue",
-        "TSRainbowViolet",
-      },
-    }
-  end,
+  -- "nvim-treesitter/nvim-treesitter",
+  -- dependencies = { "HiPhish/nvim-ts-rainbow2" },
+  -- opts = function(_, opts)
+  --   opts.rainbow = {
+  --     enable = true,
+  --     query = "rainbow-parens",
+  --     strategy = require("ts-rainbow").strategy.global,
+  --     hlgroups = {
+  --       "TSRainbowRed",
+  --       "TSRainbowOrange",
+  --       "TSRainbowYellow",
+  --       "TSRainbowGreen",
+  --       "TSRainbowBlue",
+  --       "TSRainbowViolet",
+  --     },
+  --   }
+  -- end,
 }
