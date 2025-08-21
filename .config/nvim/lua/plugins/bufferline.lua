663d719cd70f6ab3749feb51b92ef9d7c10041ef
Sacrond <bryanphui@gmail.com>
2025-08-21 20:51:38 +0800

    fix: bufferline issue

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,12 @@
 return {
     {
         "akinsho/bufferline.nvim",
+        init = function()
+            local bufline = require("catppuccin.groups.integrations.bufferline")
+            function bufline.get()
+                return bufline.get_theme()
+            end
+        end,
         opts = function(_, opts)
             opts.options.separator_style = "slant"
         end,
